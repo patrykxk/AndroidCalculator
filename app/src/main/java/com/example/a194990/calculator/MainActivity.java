d71c1b1a80ff099d875e7d633fadaa10bdcd246d
patrykxk <patrix215@gmail.com>
2017-03-16 21:13:16 +0100

    Calculating two numbers

--- app/src/main/java/com/example/a194990/calculator/MainActivity.java
+++ app/src/main/java/com/example/a194990/calculator/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.a194990.calculator;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private String onScreen = "";
+    private String currentOperator = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +23,56 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        textView = (TextView)findViewById(R.id.textView);
+        textView.setText(onScreen);
     }
 
+    public void onClickNumber(View view){
+        Button button = (Button)view;
+        onScreen += button.getText();
+        showOnScreen();
+    }
+
+    public void onClickOperator(View view){
+        Button button = (Button)view;
+        onScreen += button.getText();
+        currentOperator = button.getText().toString();
+        showOnScreen();
+    }
+    public void onClickEqual(View view){
+        Button button = (Button)view;
+        String[] numbers = onScreen.split(Pattern.quote(currentOperator));
+        if(numbers.length<2) return;
+        onScreen = String.valueOf(calculate(numbers[0],numbers[1], currentOperator));
+
+        showOnScreen();
+    }
+
+    public void onClickBackspace(View view){
+
+
+        onScreen = onScreen.substring(0, onScreen.length()-1);
+        showOnScreen();
+    }
+    public void onClickClear(View view){
+        onScreen = "";
+        currentOperator = "";
+        showOnScreen();
+    }
+
+    public void showOnScreen(){
+        textView.setText(onScreen);
+    }
+
+    private double calculate(String firstNumber, String secondNumber, String operator){
+        switch (operator){
+            case "+": return Double.valueOf(firstNumber) + Double.valueOf(secondNumber);
+            case "-": return Double.valueOf(firstNumber) - Double.valueOf(secondNumber);
+            case "*": return Double.valueOf(firstNumber) * Double.valueOf(secondNumber);
+            case "/": return Double.valueOf(firstNumber) / Double.valueOf(secondNumber);
+            default: return -1;
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
