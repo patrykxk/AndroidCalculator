8a6b3791e25481fe3ad97a43a85f852a4065e2b1
Patryk K <patrykxk@users.noreply.github.com>
2017-04-28 00:39:45 -0700

    Update ExampleUnitTest.java
    checking AstroCalculator

--- app/src/test/java/com/example/a194990/calculator/ExampleUnitTest.java
+++ app/src/test/java/com/example/a194990/calculator/ExampleUnitTest.java
@@ -12,6 +12,23 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        int month = Calendar.getInstance().get(Calendar.MONTH)+1;
+        int day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+        int hour = Calendar.getInstance().get(Calendar.HOUR);
+        int minute = Calendar.getInstance().get(Calendar.MINUTE);
+        int second = Calendar.getInstance().get(Calendar.SECOND);
+
+        int timezoneOffset = 1;
+        boolean daylightSaving = true;
+
+        AstroDateTime astroDateTime = new AstroDateTime(year,month,day,hour,minute,second,timezoneOffset,daylightSaving);
+        AstroCalculator astro = new AstroCalculator(astroDateTime, new AstroCalculator.Location(51.170,19.459) );
+        AstroCalculator.MoonInfo moonInfo = astro.getMoonInfo();
+
+        System.out.println("Ilumination: " + moonInfo.getIllumination());
+
+        System.out.println("age: " + moonInfo.getAge());
+        System.out.println(astro.getSunInfo().getSunrise().toString());
     }
 }
